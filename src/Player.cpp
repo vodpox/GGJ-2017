cf9e1cb3d35d709b9f50dc117716ad20bf9e788a
Lukas <theluksys3@gmail.com>
2017-01-21 03:24:14 +0200

    Adding player aiming

---
+++ src/Player.cpp
@@ -0,0 +1,87 @@
+#include "Game.h"
+#include "Player.hpp"
+#include <cmath>
+
+Player::Player(tplay::Game *game){
+	this->game = game;
+	setXY(x, y);
+}
+
+int Player::getX() {
+	return x;
+}
+
+int Player::getY() {
+	return y;
+}
+
+void Player::changeHealth(int change) {
+	health += change;
+}
+
+void Player::changeJammerCount(int change) {
+	jammerCount += change;
+}
+
+int Player::getHealth() {
+	return health;
+}
+
+void Player::move(int x, int y) {
+	this->x += x;
+	this->y += y;
+}
+
+void Player::moveAim(int x, int y) {
+	this->xAim += x;
+	this->yAim += y;
+}
+
+void Player::setXY(int x, int y) {
+	this->x = x;
+	this->y = y;
+}
+void Player::update(){
+    if (game->input.isButtonDown(tplay::Keyboard::S)) {
+		if(isAiming) moveAim(0, -1);
+		else move(0, -1);
+	}else if(game->input.isButtonDown(tplay::Keyboard::W)){
+		if(isAiming) moveAim(0, 1);
+		else move(0, 1);
+	}else if(game->input.isButtonDown(tplay::Keyboard::A)){
+		if(isAiming) moveAim(-1, 0);
+		else move(-1, 0);
+	}else if(game->input.isButtonDown(tplay::Keyboard::D)){
+		if(isAiming) moveAim(1, 0);
+		else move(1, 0);
+	}
+	
+	if(game->input.isButtonDown(tplay::Keyboard::SPACEBAR)){
+		xAim = x;
+		yAim = y;
+		if(isAiming){
+			isAiming = false;
+		}else{
+			isAiming = true;
+		}
+	}
+}
+
+void Player::draw(){
+	game->graphics.addToWorld(x, y, "@");
+	if(isAiming){
+		drawAim();
+	}
+}
+
+void Player::drawAim(){
+	for (int aimy = 0; aimy <= 2*aimRadius; aimy++){
+		for (int aimx = 0; aimx <= 2*aimRadius; aimx++){
+			distance_to_centre = sqrt((aimy - aimRadius)*(aimy - aimRadius) + (aimx - aimRadius)*(aimx - aimRadius));
+			if (distance_to_centre > aimRadius-0.5 && distance_to_centre < aimRadius+0.5){
+				game->graphics.addToWorld(aimx+x-aimRadius, aimy+y-aimRadius, ".");
+			}
+		}
+	}
+	game->graphics.addToWorld(xAim, yAim, "x");
+}
--- src/Player.hpp
+++ src/Player.hpp
@@ -5,12 +5,17 @@
 
 class Player {
 	private:
-		int x = 5;
-		int y = 5;
+		int x = 15;
+		int y = 15;
+		int xAim = x;
+		int yAim = y;
 		int health = 100;
 		int maxHealth = 100;
 		int jammerCount = 0;
 		int maxJammerCount = 3;
+		int aimRadius = 6;
+		bool isAiming = false;
+		float distance_to_centre;
 		tplay::Game *game;
 	
 	public:
@@ -22,9 +27,10 @@ class Player {
 		void changeJammerCount(int change);
 		void setXY(int x, int y);
 		void move(int x, int y);
+		void moveAim(int x, int y);
 		void update();
 		void draw();
-
+		void drawAim();
 };
 
 #endif
